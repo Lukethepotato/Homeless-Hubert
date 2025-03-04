b37f73968ca85aadb5b0897ffc60ac11fabb2494
Lukethepotato <pearcebrothers2@gmail.com>
2025-03-04 10:53:20 -0800

    Made some starting nodes

---
+++ Resources/battle_scenario.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name battle_scenario
+
+@export var enemy_node: NodePath
+@export var battle_overlay: NodePath
---
+++ Resources/enemy_attack_resource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name enemy_attack
+
+@export var damage: int
+@export var animation_name: String
