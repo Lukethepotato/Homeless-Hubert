66a03fa03b804c31bd7593406e13be69bc85cd2f
Lukethepotato <pearcebrothers2@gmail.com>
2025-03-05 21:50:04 -0800

    Todays final push bobby pearce is here
    good song https://open.spotify.com/track/4w9soAM7IrmYDhSXLp14p6?si=d44c48fe56f54f85

--- prephabs/main_dock_animation_player.gd
+++ prephabs/main_dock_animation_player.gd
@@ -14,8 +14,8 @@ func _process(delta: float) -> void:
 
 
 func _on_animation_finished(anim_name: StringName) -> void:
-	if anim_name == PlayerAutoload.attack_resources_in[0].animation_name:
+	if anim_name == PlayerAutoload.attack_resources_in[0].animation_name && PlayerAutoload.attack_resources_in[1] != null:
 		play(PlayerAutoload.attack_resources_in[1].animation_name)
 		
-	elif anim_name == PlayerAutoload.attack_resources_in[1].animation_name:
+	else:
 		GlobalsAutoload.current_turn = 4 # Replace with function body.
---
+++ prephabs/misc_player_anim_player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+#Jermey can you put an enum here for the states of movment
+#and make the animation player play them based on them
+#make sure to make him idle when hes in a fight by using the global autoload
+#dont want to make hubert move left and right during the enemys turn
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
