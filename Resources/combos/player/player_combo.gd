2eebdc0253e063a6e8254795021245a9789e9eb7
Lukethepotato <pearcebrothers2@gmail.com>
2025-03-08 02:28:09 -0800

    combo buggy but kinda works

--- Resources/combos/player/player_combo.gd
+++ Resources/combos/player/player_combo.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name player_combo
 
 @export var attacks_in_combo: Array[player_attack]
+@export var animation_name: String = ""
--- prephabs/main_dock_animation_player.gd
+++ prephabs/main_dock_animation_player.gd
@@ -16,9 +16,6 @@ func _process(delta: float) -> void:
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	if (PlayerAutoload.attack_resources_in.size() > attack_in_turn_index_finished) && PlayerAutoload.attack_resources_in[0] != null:
-		
-		#check if a combo was done here and if so dont play the next attack in turn index play combo
-		
 		play(PlayerAutoload.attack_resources_in[attack_in_turn_index_finished].animation_name)
 		attack_in_turn_index_finished += 1
 		print("attack played")
@@ -28,13 +25,33 @@ func _on_animation_finished(anim_name: StringName) -> void:
 		GlobalsAutoload.current_turn += 1
 		
 	
-
+func combo_checking() -> player_combo:
+	for i in GlobalsAutoload.combo_node.combo_resources.size():
+		var times_while_loop_ran: int = 0
+		for b in range(GlobalsAutoload.combo_node.combo_resources[i].attacks_in_combo.size()-1, -1,-1):
+			print("ran through " + str(b))
+			times_while_loop_ran += 1
+			if b == 0:
+				print(GlobalsAutoload.combo_node.combo_resources[i].animation_name)
+				return GlobalsAutoload.combo_node.combo_resources[i]
+			elif GlobalsAutoload.combo_node.combo_resources[i].attacks_in_combo[b] != PlayerAutoload.attack_history[PlayerAutoload.attack_history.size()- times_while_loop_ran]:
+				i += 1
+				break
+				print("checked combo not done attacks")
+	return null
+			
+		
 
 func _on_animation_started(anim_name: StringName) -> void:
 	if (PlayerAutoload.attack_resources_in.size() > attack_in_turn_index_started) && PlayerAutoload.attack_resources_in[0] != null:
 		PlayerAutoload.attack_history.append(PlayerAutoload.attack_resources_in[attack_in_turn_index_started])
-		
+		print("animation start")
 		#check if a combo was done here and if so dont play the next attack instead play combo
+		var temp_combo_check = combo_checking()
+		if temp_combo_check != null:
+			play(temp_combo_check.animation_name)
+			
+		
 		
 		attack_in_turn_index_started += 1
 	else:
