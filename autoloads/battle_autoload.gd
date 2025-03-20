b86c67b271e2761fc06fdf8e20cbbdc34c7e50d3
baconmoo <jerleopard@gmail.com>
2025-03-19 23:32:02 -0700

    started something
    ahhahaha

--- autoloads/battle_autoload.gd
+++ autoloads/battle_autoload.gd
@@ -20,9 +20,26 @@ enum ailments {
 	DAZE,
 }
 
+# Returns a two-item array in the format of [User, Target]
+func convert_strs_to_attack_roles(user : String, target : String) -> Array:
+	var perpetrator;
+	var victim;
+	
+	# This section figures out what exactly the User and Target strings denote
+	if (user.to_lower() == "player"):
+		perpetrator = PlayerAutoload;
+	else:
+		perpetrator = GlobalsAutoload.enemy_node;
+	if (target.to_lower() == "player"):
+		victim = PlayerAutoload;
+	else:
+		victim = GlobalsAutoload.enemy_node;
+	
+	return [perpetrator, victim]
+
 # Calculates the damage that should be dealt. Extraneous parameters to be placed after the first three
 func calculate_damage(base_dmg : int, user, target, can_crit := true, guaranteed_hit := false) -> int:
-	var damage = base_dmg;
+	var damage := base_dmg;
 	#print("Base damage: " + str(damage))
 	damage += user.strength;
 	#print("Strength damage: " + str(damage))
@@ -35,3 +52,6 @@ func calculate_damage(base_dmg : int, user, target, can_crit := true, guaranteed
 		damage = 0;
 		print_rich("[color=red][shake amp=50.0 freq=5.0][font_size=20]Evaded: " + str(damage));
 	return damage;
+
+func apply_combo_effects(user : String, target : String, combo : player_combo) -> void:
+	PlayerAutoload.attack_history.clear()
--- damage_donator.gd
+++ damage_donator.gd
@@ -2,29 +2,18 @@ extends Node2D
 
 # This script contains a single "damage donation" function which takes in an integer as the damage dealt in a given attack. The function itself checks which character should receive the damage.
 
-# I rewrote this sorry; User is who is using the attack, Target is the target of the attack, Base_dmg is the attack base damage, Can_crit is if the attack can crit, 
+# User is who is using the attack, Target is the target of the attack, Base_dmg is the attack base damage, Can_crit is if the attack can crit, 
 # Guaranteed_hit is if the attack can miss
 
-func _damage_donation(user : String, target : String, base_dmg: int, can_crit := true, guaranteed_hit := false):
-	var perpetrator;
-	var victim;
+func _damage_donation(user : String, target : String, base_dmg: int, combo : player_combo = null, can_crit := true, guaranteed_hit := false):
+	var roles = BattleAutoload.convert_strs_to_attack_roles(user, target)
 	
-	# This section figures out what exactly the User and Target strings denote
-	if (user.to_lower() == "player"):
-		perpetrator = PlayerAutoload;
-	else:
-		perpetrator = GlobalsAutoload.enemy_node;
-	if (target.to_lower() == "player"):
-		victim = PlayerAutoload;
-	else:
-		victim = GlobalsAutoload.enemy_node;
+	var damage_to_deal = BattleAutoload.calculate_damage(base_dmg, roles[0], roles[1], can_crit, guaranteed_hit);
 	
-	var damage_to_deal = BattleAutoload.calculate_damage(base_dmg, perpetrator, victim, can_crit, guaranteed_hit);
-	
-	if perpetrator != victim:
-		if perpetrator.attack_history[perpetrator.attack_history.size()- 1].hit_region != victim.current_block: 
-			victim.health -= damage_to_deal;
+	if roles[0] != roles[1]:
+		if roles[0].attack_history[roles[0].attack_history.size() - 1].hit_region != roles[1].current_block: 
+			roles[1].health -= damage_to_deal;
 	else:
-		victim.health -= damage_to_deal;
+		roles[1].health -= damage_to_deal;
 	
 	GlobalsAutoload.health_updated.emit();
