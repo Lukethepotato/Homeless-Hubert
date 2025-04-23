eb29f24b29930bd04b83946bf7c5a82bbb2804e9
Lukethepotato <pearcebrothers2@gmail.com>
2025-04-23 09:20:22 -0700

    CHROMATIC ABERATION!!!!

--- prefabs/main_dock_script.gd
+++ prefabs/main_dock_script.gd
@@ -7,3 +7,11 @@ extends Node2D
 func _on_fishing_spot_body_entered(body: Node2D) -> void:
 	if body.name == "Hubert":
 		GlobalsAutoload.start_battle(battle_scenarios);
+
+func _Chromatic_func():
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(%chromatic_ab.material, "shader_parameter/chaos", 12, 0.5)
+	tween.tween_interval(.1)
+	tween.tween_property(%chromatic_ab.material, "shader_parameter/chaos", 0, 0.1)
