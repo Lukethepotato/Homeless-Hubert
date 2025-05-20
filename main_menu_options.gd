39b4c7a650db072026e7f32bfc77c6831943ab2b
baconmoo <jerleopard@gmail.com>
2025-05-20 13:19:31 -0700

    more minor main menu changes
    - rename options.gd to main_menu_options.gd
    - removed set menu (forgot to save last time)

--- main_menu_options.gd
+++ main_menu_options.gd
@@ -5,27 +5,18 @@ var music_index: int
 var sfx_index: int
 var voice_index: int
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	master_index = AudioServer.get_bus_index("Master")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-# HIIIIII MCKENZIE :))))))))
-
-
+	master_index = AudioServer.get_bus_index("Master");
+	music_index = AudioServer.get_bus_index("Music");
+	sfx_index = AudioServer.get_bus_index("Sound Effects");
+	voice_index = AudioServer.get_bus_index("Voice/Voice Effects");
 
 func _on_master_audio_bar_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(master_index, linear_to_db(value))
 
-
 func _on_music_audio_bar_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(music_index, linear_to_db(value))
-
 
 func _on_sfx_audio_bar_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(sfx_index, linear_to_db(value))
