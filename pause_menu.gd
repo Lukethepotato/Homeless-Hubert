35522e086d83af75f2d875ff6c340f519a7308ec
baconmoo <jerleopard@gmail.com>
2025-05-20 11:40:14 -0700

    fuckin done
    - pause menu works based on global function and gets instantiated into the current scene

--- pause_menu.gd
+++ pause_menu.gd
@@ -5,29 +5,32 @@ extends CanvasLayer
 var index := 0;
 var max_index : int;
 var selection : RichTextLabel;
+var tween;
+var is_ready := false;
 
 func _ready() -> void:
 	max_index = $text_nodes.get_child_count()-1;
 	selection = $text_nodes.get_child(index);
-	update_text()
+	tween_in();
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("UP"):
-		if index > 0:
-			index -= 1;
-			print("index_changed")
-		else:
-			index = max_index
-		update_text()
-	if event.is_action_pressed("DOWN"):
-		if index < max_index:
-			index += 1
-			print("index_changed")
-		else:
-			index = 0;
-		update_text()
-	if event.is_action_pressed('CONFIRM'):
-		process_selection();
+	if is_ready:
+		if event.is_action_pressed("UP"):
+			if index > 0:
+				index -= 1;
+				print("index_changed")
+			else:
+				index = max_index
+			update_text()
+		if event.is_action_pressed("DOWN"):
+			if index < max_index:
+				index += 1
+				print("index_changed")
+			else:
+				index = 0;
+			update_text()
+		if event.is_action_pressed('CONFIRM'):
+			process_selection();
 
 func update_text():
 	var selected_text_prefix = "[center][color=yellow][font_size=40] "
@@ -49,9 +52,31 @@ func process_selection():
 			GlobalsAutoload.begin_load();
 			await GlobalsAutoload.overlay_done
 			get_tree().change_scene_to_file("res://main_menu.tscn")
+			GlobalsAutoload.state = GlobalsAutoload.game_states.ROAMING
+			queue_free()
 
 func tween_in():
-	pass
+	if tween:
+		tween.kill();
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_parallel(true);
+	tween.tween_property($background, "modulate:a", 1, 0.5).from(0);
+	tween.tween_property($paused, "position:y", 130, 0.5).from(-200);
+	tween.tween_property($paused, "modulate:a", 1, 0.5).from(0);
+	tween.tween_property($text_nodes, "position:y", 304, 0.5).from(650);
+	tween.tween_property($text_nodes, "modulate:a", 1, 0.5).from(0);
+	await tween.finished
+	update_text()
+	is_ready = true;
 
 func tween_out():
-	pass
+	if tween:
+		tween.kill();
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_parallel(true);
+	tween.tween_property($background, "modulate:a", 0, 0.5);
+	tween.tween_property($paused, "position:y", -200, 0.5);
+	tween.tween_property($paused, "modulate:a", 0, 0.5)
+	tween.tween_property($text_nodes, "position:y", 650, 0.5)
+	tween.tween_property($text_nodes, "modulate:a", 0, 0.5)
+	await tween.finished
+	GlobalsAutoload.state = GlobalsAutoload.game_states.ROAMING
+	queue_free()
